e10f0f29e6f7d69913a1eb705d2b1acb9a6bc92d
Piotr Modes <piotr.modes@lifeinmobile.com>
2020-08-29 10:54:58 +0200

    create dynamic route for todo details

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        main
+        <Link as="todo-details/1234" href="/todo-details/[id]">
+          <a>details</a>
+        </Link>
+      </main>
     </div>
   );
 }
---
+++ src/pages/todo-details/[id].js
@@ -0,0 +1,5 @@
+const SingleTodoDetails = () => {
+  return <h1>details</h1>;
+};
+
+export default SingleTodoDetails;
